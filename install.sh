0079509d453dba9296fb59aa8e4579859c2e0eb9
Dayal Mukati <42741864+DayalMukati@users.noreply.github.com>
2019-12-05 14:08:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,22 +26,6 @@ set -e
 # Array of supported versions
 declare -a versions=('trusty' 'debian' 'yakkety', 'bionic');
 
-# check the version and extract codename of ubuntu if release codename not provided by user
-if [ -z "$1" ]; then
-    source /etc/lsb-release || \
-        (echo "Error: Release information not found, run script passing Ubuntu version codename as a parameter"; exit 1)
-    CODENAME=${DISTRIB_CODENAME}
-else
-    CODENAME=${1}
-fi
-
-# check version is supported
-if echo ${versions[@]} | grep -q -w ${CODENAME}; then
-    echo "Installing Hyperledger Composer prereqs for Ubuntu ${CODENAME}"
-else
-    echo "Error: Ubuntu ${CODENAME} is not supported"
-    exit 1
-fi
 
 # Update package lists
 echo "# Updating package lists"
